dad96c79fcd94c9e51630be1bf217b59b471497b
Maitrey185 <maitreypatil185@gmail.com>
2021-05-23 14:15:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function chordifyy(chord){
+  if(chord==='Cmaj'){
+    return ['C','E','G'];
+  }
+}
+//console.log(getKeys('Cmaj'));
+//export { getKeys as default };
+module.export.chordifyy = chordifyy;
